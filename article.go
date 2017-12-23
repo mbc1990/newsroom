f10cf1e73e9c04b2ed9bc132961a4d5c55a0b5eb
mbc <malcolmbalch@gmail.com>
2017-12-23 16:40:39 -0500

    Save tokenized articles to disk

--- article.go
+++ article.go
@@ -2,25 +2,28 @@ package main
 
 import "os"
 import "io/ioutil"
+import "encoding/json"
 import "strings"
 import "strconv"
 
 // Represents an individual Article
 type Article struct {
-	Id         int // UUID for article
-	Headline   string
-	Body       string         // Unmanipulated text
-	Tokens     *[]string      // Tokenized, in order text
-	BagOfWords map[string]int // Tokenized, stopwords removed, word/count vector
+	Id               int // UUID for article
+	Headline         string
+	Body             string         // Unmanipulated text
+	Tokens           *[]string      // Tokenized, in order text
+	BagOfWords       map[string]int // Tokenized, stopwords removed, word/count vector
+	TokenizerVersion int            // Used to invalidate saved tokens made with an old version
 }
 
 // Initialize article for manipulation
 func (a *Article) Initialize(item FeedItem, savedTextDir string) {
+	a.TokenizerVersion = 1
 	a.Id = item.Id
 	a.Headline = item.Headline
 	a.PopulateBody(savedTextDir)
-	a.PopulateTokens()
-	a.PopulateBoW()
+	a.PopulateTokens(savedTextDir)
+	a.PopulateBoW(savedTextDir)
 }
 
 // Not all articles will have a body due to failed or queued scraping jobs
@@ -44,16 +47,40 @@ func (a *Article) PopulateBody(savedTextDir string) {
 }
 
 // Tokenizes headline + body (if it exists)
-func (a *Article) PopulateTokens() {
+func (a *Article) PopulateTokens(savedTextDir string) {
+	// If we already tokenized this article, read it from disk
+	path := savedTextDir + strconv.Itoa(a.Id) + ".tokens." + strconv.Itoa(a.TokenizerVersion) + ".json"
+	if _, err := os.Stat(path); err == nil {
+		bytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+		var tokens []string
+		json.Unmarshal(bytes, &tokens)
+		a.Tokens = &tokens
+		return
+	}
+
+	// Tokens aren't cached, so tokenize the article and write the tokens to disk
 	toTokenize := a.Headline
 	if a.HasBody() {
 		toTokenize = toTokenize + " " + a.Body
 	}
 	a.Tokens = RemoveStopWords(Tokenize(RemovePunctuation(strings.ToLower(toTokenize))))
+	bytes, err := json.Marshal(a.Tokens)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Create(path)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	file.WriteString(string(bytes))
 }
 
 // Populates BagOfWords
-func (a *Article) PopulateBoW() {
+func (a *Article) PopulateBoW(savedTextDir string) {
 	a.BagOfWords = make(map[string]int)
 	for _, token := range *a.Tokens {
 		_, ok := a.BagOfWords[token]
